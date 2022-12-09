f3f7268d104251f98814d2a734b18c67e7bf4191
Iqrazamir1 <zamiriqra0@outlook.com>
2022-12-09 19:34:02 +0000

    day 8 complete

---
+++ JavaIntermediate/src/jdbc/DeleteRecords.java
@@ -0,0 +1,29 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DeleteRecords {
+public static void main(String[] args) {
+		
+		try {
+			String url = "jdbc:mysql://localhost:3306/javadb";
+			String userName = "root";
+			String password = "Football1";
+			Connection con = DriverManager.getConnection(url, userName, password);
+			System.out.println("Connected successfully");
+			
+			// Delete data in the database table 
+			String query = "update customer set phone = 45647885 where id = 103";
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate(query);
+			System.out.println("One record updated successfully!");
+
+		} catch (Exception e) {
+			System.out.println("Something went wrong");
+			System.out.println(e.getMessage());
+		}
+ 
+	}
+}
